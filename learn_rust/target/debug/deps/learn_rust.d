a50c51cad32c8db7f4e8bb17105bc47324cbdf9c
Domagoj Ratko <domagoj.ratko@gmail.com>
2022-07-09 22:57:32 +0200

    Updated readme.md working on version 0.5 - Constant ✅ - String ❌ - Operators ❌ - Decision Making ❌ - Loop ❌ - Functions ❌ - Tuple ❌ - Array ❌ - Ownership ❌ - Borrowing ❌ - Slices ❌ - Structure ❌

--- learn_rust/target/debug/deps/learn_rust.d
+++ learn_rust/target/debug/deps/learn_rust.d
@@ -1,7 +1,8 @@
-C:\Rust\Learn-Rust\learn_rust\target\debug\deps\learn_rust.exe: src\main.rs src\data_types.rs src\variables.rs
+C:\Rust\Learn-Rust\learn_rust\target\debug\deps\learn_rust.exe: src\main.rs src\data_types.rs src\variables.rs src\constant.rs
 
-C:\Rust\Learn-Rust\learn_rust\target\debug\deps\learn_rust.d: src\main.rs src\data_types.rs src\variables.rs
+C:\Rust\Learn-Rust\learn_rust\target\debug\deps\learn_rust.d: src\main.rs src\data_types.rs src\variables.rs src\constant.rs
 
 src\main.rs:
 src\data_types.rs:
 src\variables.rs:
+src\constant.rs:
