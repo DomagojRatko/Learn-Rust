2f06f9ff806a4509bb6898772594aa5e4410030f
Domagoj Ratko <domagoj.ratko@gmail.com>
2022-07-26 11:04:27 +0200

    working on version 1.0 updated README.md - Generic Types - Input Output - File Input/ Output - Package Manager - Iterator and Closure - Smart Pointers - Concurrency

--- learn_rust/target/debug/deps/learn_rust.d
+++ learn_rust/target/debug/deps/learn_rust.d
@@ -1,6 +1,6 @@
-C:\Rust\Learn-Rust\learn_rust\target\debug\deps\learn_rust.exe: src\main.rs src\data_types.rs src\variables.rs src\constant.rs src\string.rs src\decision_making.rs src\loops.rs src\functions.rs src\tuple.rs src\arrays.rs src\ownership.rs src\borrowing.rs src\slices.rs src\structure.rs src\enums.rs src\module.rs src\example\movie.rs src\vectors.rs src\hashmap.rs src\hashset.rs
+C:\Rust\Learn-Rust\learn_rust\target\debug\deps\learn_rust.exe: src\main.rs src\data_types.rs src\variables.rs src\constant.rs src\string.rs src\decision_making.rs src\loops.rs src\functions.rs src\tuple.rs src\arrays.rs src\ownership.rs src\borrowing.rs src\slices.rs src\structure.rs src\enums.rs src\module.rs src\example\movie.rs src\vectors.rs src\hashmap.rs src\hashset.rs src\error_handling.rs
 
-C:\Rust\Learn-Rust\learn_rust\target\debug\deps\learn_rust.d: src\main.rs src\data_types.rs src\variables.rs src\constant.rs src\string.rs src\decision_making.rs src\loops.rs src\functions.rs src\tuple.rs src\arrays.rs src\ownership.rs src\borrowing.rs src\slices.rs src\structure.rs src\enums.rs src\module.rs src\example\movie.rs src\vectors.rs src\hashmap.rs src\hashset.rs
+C:\Rust\Learn-Rust\learn_rust\target\debug\deps\learn_rust.d: src\main.rs src\data_types.rs src\variables.rs src\constant.rs src\string.rs src\decision_making.rs src\loops.rs src\functions.rs src\tuple.rs src\arrays.rs src\ownership.rs src\borrowing.rs src\slices.rs src\structure.rs src\enums.rs src\module.rs src\example\movie.rs src\vectors.rs src\hashmap.rs src\hashset.rs src\error_handling.rs
 
 src\main.rs:
 src\data_types.rs:
@@ -22,3 +22,4 @@ src\example\movie.rs:
 src\vectors.rs:
 src\hashmap.rs:
 src\hashset.rs:
+src\error_handling.rs:
